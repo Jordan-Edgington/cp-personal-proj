d3fc9bd3aebdd22c527c37d71fb8f237a8b81ede
Jordan Edgington <jordan.edgington.dev@gmail.com>
2024-04-05 18:13:04 -0400

    add: signup funct to front-end

--- front-end/src/pages/SignupPage.jsx
+++ front-end/src/pages/SignupPage.jsx
@@ -1,26 +1,36 @@
-import { Button } from "@/components/ui/button"
+import { useState } from 'react'
+import { api } from '../utilities.jsx'
 /* eslint-disable react/no-unescaped-entities */
 function SignupPage() {
+      const [emailInput, setEmailInput] = useState('')
+      const [displayNameInput, setDisplayNameInput] = useState('')
+      const [passwordInput, setPasswordInput] = useState('')
 
+      const handleSignupForm = async(e) => {
+            e.preventDefault()
+            const response = await api.post("users/signup/",{email:emailInput, display_name:displayNameInput, password:passwordInput})
+            if (response.status === 201) {
+                  console.log('successfully signed up, user info', response.data)
+      }}
   return (
    <>
    <div className='flex flex-col justify-center items-center text-center w-1/2 m-24'>
         <p className='text-2xl mb-4'>Create your Account</p>
-        <form className='flex flex-col w-1/2'>
+        <form className='flex flex-col w-1/2' onSubmit={handleSignupForm}>
             <div className='flex flex-row justify-end mr-10'>
                 <p>Display Name</p>
-                <input type='text' className='border-2 rounded border-black m-1'></input>
+                <input type='text' className='border-2 rounded border-black m-1' onChange={(e)=>setDisplayNameInput(e.target.value)}></input>
             </div>
           <div className='flex flex-row justify-end mr-10'>
                 <p>Email</p>
-                <input type='text' className='border-2 rounded border-black m-1'></input>
+                <input type='text' className='border-2 rounded border-black m-1' onChange={(e)=>setEmailInput(e.target.value)}></input>
           </div>
           <div className='flex flex-row justify-end mr-10'>
                 <p>Password</p>
-                <input type='text' className='border-2 rounded border-black m-1'></input>
+                <input type='text' className='border-2 rounded border-black m-1' onChange={(e)=>setPasswordInput(e.target.value)}></input>
           </div>
           <div>
-                <Button className='bg-orange-900 border-2 border-black'>Log In</Button>
+                <input type='submit' value='Sign Up' className='bg-orange-900 border-2 border-black rounded p-1 text-white'/>
           </div>
         </form>
       </div>

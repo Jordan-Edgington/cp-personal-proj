cba7cb691b48d68663b824c1e2f8864a0c517262
Jordan Edgington <jordan.edgington.dev@gmail.com>
2024-04-16 11:20:27 -0400

    edit userConfirmation

--- front-end/src/utilities.jsx
+++ front-end/src/utilities.jsx
@@ -6,20 +6,29 @@ export const api = axios.create({
     withCredentials:true
 })
 
-export const userConfirmation = async() => {
-    // const token = localStorage.getItem("token");
-    // if (token) {
-        // api.defaults.headers.common["Authorization"] = `Token ${token}`
-        const response = await api.get('users/info/')
+export const userConfirmation = async () => {
+    try {
+        // const token = localStorage.getItem("token");
+        // if (token) {
+        //     api.defaults.headers.common["Authorization"] = `Token ${token}`
+        // }
+        
+        const response = await api.get('users/info/');
+        console.log('response', response);
+        
         if (response.status === 200) {
-            console.log(response.data)
-            return { user: response.data.user, email: response.data.email, display_name:response.data.display_name }
-        } else { 
-            console.log('error userConfirmation', response)
-            return null
+            console.log(response.data);
+            return { user: response.data.user, email: response.data.email, display_name: response.data.display_name };
+        } else {
+            console.log('Error in userConfirmation:', response);
+            return null;
         }
+    } catch (error) {
+        console.log('No token provided from cookies.', error);
+        return null;
+    }
+};
     // } else {
     //     console.log('userConfirmation: no token in localStorage')
     //     return null
     // }
-}
